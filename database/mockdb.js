704e504a3e57622acbb1ca5c74b79ef907c885bb
Bilguun Gantogos <gantogos.bilguun48@gmail.com>
2024-04-16 10:31:59 +0800

    typescript bolgov

---
+++ database/mockdb.js
@@ -0,0 +1,12 @@
+export const users = [
+  {
+    name: "The Awakening",
+    email: "Kate@gmail.com",
+    projects: [{ title: "site upgrade - summer 2024" }],
+  },
+  {
+    name: "The FAFAFA",
+    email: "fafa@gmail.com",
+    projects: [{ title: "site upgrade - summer 2024" }],
+  },
+];
